5df9d75a745bc17e96787232399eecf4c0758d18
Liu Yen-Ting <windows.linux.mac@gmail.com>
2017-04-15 00:56:23 +0800

    Move header file to include folder

--- src/DeconvLRCore.cu
+++ src/DeconvLRCore.cu
@@ -8,8 +8,9 @@ void square_array_kernel(float *a, int N)
   if (idx<N) a[idx] = a[idx] * a[idx];
 }
 
+__host__
 void square_array(float *a, int N) {
     int block_size = 4;
     int n_blocks = N/block_size + (N%block_size == 0 ? 0:1);
-    square_array_kernel<<<n_blocks, block_size>>>(a_d, N);
+    square_array_kernel<<<n_blocks, block_size>>>(a, N);
 }

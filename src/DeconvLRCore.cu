a6812a6267a7fa20de8ef0ecae3daa7ea4205e3c
Liu Yen-Ting <windows.linux.mac@gmail.com>
2017-06-05 15:58:11 +0800

    Remove test kernel

--- src/DeconvLRCore.cu
+++ src/DeconvLRCore.cu
@@ -6,18 +6,10 @@
 // standard libraries headers
 // system headers
 
-__global__
-void square_array_kernel(float *a, int N)
-{
-  int idx = blockIdx.x * blockDim.x + threadIdx.x;
-  if (idx<N) {
-      a[idx] = a[idx] * a[idx];
-  }
+DeconvLRCore::DeconvLRCore() {
+
 }
 
-__host__
-void square_array(float *a, int N) {
-    int block_size = 4;
-    int n_blocks = N/block_size + (N%block_size == 0 ? 0:1);
-    square_array_kernel<<<n_blocks, block_size>>>(a, N);
+DeconvLRCore::~DeconvLRCore() {
+
 }
--- src/DeconvLRDriver.cpp
+++ src/DeconvLRDriver.cpp
@@ -6,7 +6,6 @@
 // 3rd party libraries headers
 #include <cuda_runtime.h>
 // standard libraries headers
-#include <cstdlib>
 // system headers
 
 class DeconvLR::Impl {
@@ -18,10 +17,8 @@ public:
 
     }
 
-    void do_internal_work();
-
 private:
-    int private_int = 0;
+    DeconvLRCore device;
 };
 
 DeconvLR::DeconvLR()
@@ -32,30 +29,9 @@ DeconvLR::~DeconvLR() {
 
 }
 
-void square_calc_demo() {
-    float *a_h, *a_d;  // Pointer to host & device arrays
-    const int N = 10;  // Number of elements in arrays
-    size_t size = N * sizeof(float);
-    a_h = (float *)malloc(size);        // Allocate array on host
-
-    cudaMalloc((void **) &a_d, size);   // Allocate array on device
-    // Initialize host array and copy it to CUDA device
-    for (int i=0; i<N; i++) {
-        a_h[i] = (float)i;
-    }
-    cudaMemcpy(a_d, a_h, size, cudaMemcpyHostToDevice);
-
-    // Do calculation on device:
-    square_array(a_d, N);
-
-    // Retrieve result from device and store it in host array
-    gpuErrchk(cudaMemcpy(a_h, a_d, sizeof(float)*N, cudaMemcpyDeviceToHost));
-    // Print results
-    for (int i=0; i<N; i++) {
-        printf("%d %f\n", i, a_h[i]);
-    }
-
-    // Cleanup
-    free(a_h);
-    gpuErrchk(cudaFree(a_d));
+void DeconvLR::setOTF(
+    const ImageStack<float> &otf,
+    const float dr, const float dz
+) {
+    
 }
